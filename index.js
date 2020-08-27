b94f49623c9838214f352cf5574f2022ddec8508
David Strauch <dmaster18@gmail.com>
2020-08-27 22:16:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = function() {return tutorials.map(toTitleCase);}
+const titleCased = function() {return titleCasedTutorials}
 
 
 function toTitleCase(string) {
